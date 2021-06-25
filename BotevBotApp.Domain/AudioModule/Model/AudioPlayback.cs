8807898691fb0d6a3472df1acfb542c680f5a594
Dimitar Krastev <dimi1010100@gmail.com>
2021-06-26 01:21:25 +0300

    Added a wrapper for AudioPlayback that implements a cache over the playback stream.

--- BotevBotApp.Domain/AudioModule/Model/AudioPlayback.cs
+++ BotevBotApp.Domain/AudioModule/Model/AudioPlayback.cs
@@ -10,13 +10,73 @@ namespace BotevBotApp.Domain.AudioModule.Model
         /// <summary>
         /// Gets the stream on which the playback will be outputted.
         /// </summary>
-        public Stream AudioOutputStream { get; }
+        public Stream AudioOutputStream { get; protected set; }
 
         /// <summary>
         /// Starts outputting the playback on the AudioOutputStream.
         /// </summary>
         /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is System.Threading.CancellationToken.None.</param>
-        /// <returns>A task representing the playback operation.</returns>
+        /// <returns>
+        /// A task representing the playback operation.<br/>
+        /// The task completes when the entire playback is written to the output stream.
+        /// </returns>
         public abstract Task StartAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Wraps the current <see cref="AudioPlayback"/> instance into <see cref="CachedAudioPlayback"/>.
+        /// </summary>
+        /// <returns>An new <see cref="CachedAudioPlayback"/> instance made from the current instance.</returns>
+        internal virtual CachedAudioPlayback WithCache()
+        {
+            return new CachedAudioPlayback(this);
+        }
+    }
+
+    /// <summary>
+    /// Wrapper over an <see cref="AudioPlayback"/> that caches the playback <see cref="Stream"/> into a <see cref="MemoryStream"/>.
+    /// </summary>
+    internal sealed class CachedAudioPlayback : AudioPlayback
+    {
+        private readonly AudioPlayback innerPlayback;
+
+        /// <summary>
+        /// Gets weather the playback stream has been cached.
+        /// </summary>
+        public bool Cached { get; private set; } = false;
+
+        public CachedAudioPlayback(AudioPlayback playback)
+        {
+            innerPlayback = playback;
+            AudioOutputStream = new MemoryStream();
+        }
+
+        /// <inheritdoc/>
+        public override async Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (Cached)
+            {
+                AudioOutputStream.Position = 0;
+            }
+            else
+            {
+                var innerStart = innerPlayback.StartAsync(cancellationToken);
+                await innerPlayback.AudioOutputStream.CopyToAsync(AudioOutputStream, cancellationToken).ConfigureAwait(false);
+                await innerStart;
+                Cached = true;
+            }
+        }
+
+        /// <summary>
+        /// Overrides the <see cref="AudioPlayback.WithCache()"/> method so it returns the current instance.
+        /// </summary>
+        /// <returns>The current instance.</returns>
+        /// <remarks>
+        /// As the current playback is already cached, it is redundant to create a cache of a cache.
+        /// </remarks>
+        internal override CachedAudioPlayback WithCache()
+        {
+            return this;
+        }
     }
 }

0da79bda4848ea6b48238ffeadd4a24e24aa75c4
Dimitar Krastev <dimi1010100@gmail.com>
2021-06-27 17:19:28 +0300

    Moved StoredAudioRequest to its own file. Expanded AudioRequest.

--- BotevBotApp.Domain/AudioModule/Model/AudioRequest.cs
+++ BotevBotApp.Domain/AudioModule/Model/AudioRequest.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using BotevBotApp.Domain.AudioModule.DTO;
 using BotevBotApp.Domain.Model;
-using FileStorageProviders;
 
 namespace BotevBotApp.Domain.AudioModule.Model
 {
@@ -11,37 +10,36 @@ namespace BotevBotApp.Domain.AudioModule.Model
         /// <summary>
         /// Gets the name of the requester.
         /// </summary>
-        public string Requester { get; protected set; }
+        public string Requester { get; private init; }
+
+        public AudioRequest(string requester)
+        {
+            Requester = requester;
+        }
 
         /// <summary>
         /// Gets an <see cref="AudioPlayback"/> object representing the requested audio.
         /// </summary>
         /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is System.Threading.CancellationToken.None.</param>
         /// <returns>A task representing the fetching operation.</returns>
-        public abstract Task<AudioPlayback> GetAudioPlaybackAsync(CancellationToken cancellationToken);
+        public abstract Task<AudioPlayback> GetAudioPlaybackAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets a <see cref="AudioPlayback"/> object representing the requested audio.
+        /// </summary>
+        /// <param name="cachePlayback">Determines if the returned <see cref="AudioPlayback"/> should have caching enabled.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is System.Threading.CancellationToken.None.</param>
+        /// <returns>A task representing the fetching operation.</returns>
+        public async Task<AudioPlayback> GetAudioPlaybackAsync(bool cachePlayback = false, CancellationToken cancellationToken = default)
+        {
+            var playback = await GetAudioPlaybackAsync(cancellationToken);
+            return cachePlayback ? playback.WithCache() : playback;
+        }
 
         /// <summary>
         /// Gets an audio item dto that describes the request.
         /// </summary>
         /// <returns>An audio item dto.</returns>
         public abstract AudioItemDTO ToAudioItem();
-    }
-
-    internal class StoredAudioRequest : AudioRequest
-    {
-        private readonly IFileStorageProvider storageProvider;
-        private readonly long fileId;
-
-        public override async Task<AudioPlayback> GetAudioPlaybackAsync(CancellationToken cancellationToken)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            var result = await storageProvider.GetFileDataAsync(fileId);
-            return new DecodingAudioPlayback(result).WithCache();
-        }
-
-        public override AudioItemDTO ToAudioItem()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
---
+++ BotevBotApp.Domain/AudioModule/Model/StoredAudioRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BotevBotApp.Domain.AudioModule.DTO;
+using FileStorageProviders;
+
+namespace BotevBotApp.Domain.AudioModule.Model
+{
+    internal class StoredAudioRequest : AudioRequest
+    {
+        private readonly IFileStorageProvider storageProvider;
+        private readonly long fileId;
+
+        public StoredAudioRequest(IFileStorageProvider storageProvider, long fileId, string requester) : base(requester)
+        {
+            this.storageProvider = storageProvider;
+            this.fileId = fileId;
+        }
+
+        public override async Task<AudioPlayback> GetAudioPlaybackAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await storageProvider.GetFileDataAsync(fileId, cancellationToken);
+            return new DecodingAudioPlayback(result);
+        }
+
+        public override AudioItemDTO ToAudioItem()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

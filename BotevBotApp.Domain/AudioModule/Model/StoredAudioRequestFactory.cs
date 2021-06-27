35defc7ca334a18d60e7af9ea8a29c0a8fa934fc
Dimitar Krastev <dimi1010100@gmail.com>
2021-06-27 17:19:48 +0300

    Added factory for StoredAudioRequest.

---
+++ BotevBotApp.Domain/AudioModule/Model/StoredAudioRequestFactory.cs
@@ -0,0 +1,16 @@
+﻿using FileStorageProviders;
+
+namespace BotevBotApp.Domain.AudioModule.Model
+{
+    internal class StoredAudioRequestFactory
+    {
+        private readonly IFileStorageProvider storageProvider;
+
+        public StoredAudioRequestFactory(IFileStorageProvider storageProvider)
+        {
+            this.storageProvider = storageProvider;
+        }
+
+        public StoredAudioRequest CreateAudioRequest(long fileId, string requester) => new StoredAudioRequest(storageProvider, fileId, requester);
+    }
+}

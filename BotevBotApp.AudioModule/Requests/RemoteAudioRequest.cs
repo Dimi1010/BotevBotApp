a32fda1fb1b557105caf61922c37ebc2663d46a4
Dimitar Krastev <dimi1010100@gmail.com>
2021-06-27 22:31:13 +0300

    Added basic logic for streaming audio from youtube.

---
+++ BotevBotApp.AudioModule/Requests/RemoteAudioRequest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BotevBotApp.AudioModule.Requests
+{
+    /// <summary>
+    /// Represents an audio request to a remote source.
+    /// </summary>
+    public abstract class RemoteAudioRequest : AudioRequest
+    {
+        protected Uri Url { get; private set; }
+
+        protected RemoteAudioRequest(string requester, Uri url) : base(requester)
+        {
+            if (ValidateUrl(url))
+                throw new InvalidUrlException($"The provided url: {url} is invalid.");
+            Url = url;
+        }
+
+        /// <summary>
+        /// Validates the url.
+        /// </summary>
+        /// <param name="url">The url to validate.</param>
+        /// <returns>True if the url is valid, false otherwise.</returns>
+        protected virtual bool ValidateUrl(Uri url)
+        {
+            return true;
+        }
+    }
+
+
+    [Serializable]
+    public class InvalidUrlException : Exception
+    {
+        public InvalidUrlException() { }
+        public InvalidUrlException(string message) : base(message) { }
+        public InvalidUrlException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidUrlException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ BotevBotApp.AudioModule/Requests/YoutubeAudioRequest.cs
@@ -0,0 +1,48 @@
+﻿using BotevBotApp.AudioModule.DTO;
+using BotevBotApp.AudioModule.Playback;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace BotevBotApp.AudioModule.Requests
+{
+    public class YoutubeAudioRequest : RemoteAudioRequest
+    {
+        public YoutubeAudioRequest(string requester, Uri url) : base(requester, url)
+        {
+        }
+
+        protected override bool ValidateUrl(Uri url)
+        {
+            // TODO: Add Validation.
+            return base.ValidateUrl(url);
+        }
+
+        public override async Task<AudioPlayback> GetAudioPlaybackAsync(CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var youtube = YouTube.Default;
+            var videos = await youtube.GetAllVideosAsync(Url.ToString()).ConfigureAwait(false);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var bestCandidate = videos.First(video => video.Resolution == videos.Min(v => v.Resolution));
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var videoStream = await bestCandidate.StreamAsync();
+
+            return new StreamSourceAudioPlayback(videoStream).WithCache().WithDecoding();
+        }
+
+        public override async Task<AudioItemDTO> ToAudioItemAsync(CancellationToken cancellationToken = default)
+        {
+            var youtube = YouTube.Default;
+            var video = await youtube.GetVideoAsync(Url.ToString()).ConfigureAwait(false);
+            return new AudioItemDTO { Requester = Requester, Name = video.FullName, Source = "YouTube" };
+        }
+    }
+}

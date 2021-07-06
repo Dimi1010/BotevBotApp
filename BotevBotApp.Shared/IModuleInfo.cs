f381f76b960341c0abcb685510af5ea334a1ae12
Dimitar Krastev <dimi1010100@gmail.com>
2021-07-06 18:04:41 +0300

    Added project for shared interfaces.
    (cherry picked from commit 4d94b5470533371deab733b195c5c78a7a4b3cdb)
    
    # Conflicts:
    #	src/BotevBotApp.AudioModule/BotevBotApp.AudioModule.csproj

---
+++ BotevBotApp.Shared/IModuleInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BotevBotApp
+{
+    public interface IModuleInfo
+    {
+        /// <summary>
+        /// Gets all the modules containing commands.
+        /// </summary>
+        /// <returns>An enumerable containing type information for the command modules.</returns>
+        public IEnumerable<Type> GetCommandModules();
+
+        /// <summary>
+        /// Gets the <see cref="Assembly"/> containing the module.
+        /// </summary>
+        /// <returns>The containing <see cref="Assembly"/>.</returns>
+        public Assembly GetModuleAssembly();
+    }
+}
